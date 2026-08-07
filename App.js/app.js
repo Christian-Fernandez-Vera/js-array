d9ed28ad72a6fa3b1d71e1aab68db6f1e7b8a4ea
Christian Fernandez Vera <xtian.fdz@hotmail.com>
2026-08-08 01:00:08 +0200

    feat: initialize project with basic loop exercises in app.js and index.html structure

---
+++ App.js/app.js
@@ -0,0 +1,19 @@
+/**
+ * 1. Esercizio: Stampa i numeri da 1 a 10 usando un ciclo for.
+ * 2. Stampa i numeri da 1 a 10 usando un ciclo while.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+
+
